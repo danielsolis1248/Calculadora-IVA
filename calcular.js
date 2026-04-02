d85626aaa8e0166ce19ce5d51089550ce9e73c06
danielsolis1248 <itic18.daniel.solis@iunis.edu.mx>
2026-04-02 17:56:49 -0600

    Ultimos cambios

--- calcular.js
+++ calcular.js
@@ -1,8 +1,37 @@
+const input = document.querySelectorAll(".precio");
+
+input.addEventListener('input', (e) => {
+    let valor = e.target.value.replace(/\D/g, "");
+
+    if (valor) {
+        valor = parseInt(valor).toLocaleString('en-US');
+    }
+    e.target.value = valor;
+});
+
+//Funcion para calcular el IVA
 function calcularIVA () {
-    let precio = document.querySelectorAll(".precio");
-    let porcentajeIVA = document.querySelectorAll(".IVA");
+    //Variables del precio y el portentaje de IVA
+    let precio = document.querySelectorAll('.precio');
+    let porcentajeIVA = document.querySelectorAll('.IVA');
+    
+    //Variable para almacenar el IVA del precio original
     let IVA_calculado = porcentajeIVA*precio/100;
+    
+    //Operacion para añadir el IVA al producto
     let precioFinal = precio + IVA_calculado;
 
+    //Mostrar el resultado en el modal
+    document.getElementById("resultado").textContent = "El precio final de tu producto con IVA es: " + precioFinal;
+    document.getElementById("modal").style.display = "block";
+}
 
+//Funcion para limpiar los campos de texto
+function limpiarCampos() {
+    document.querySelectorAll("input").forEach(input => input.value = "");    
+}
+
+//Funcion para cerrar modal
+function cerrarModal() {
+    document.getElementById("modal").style.display = "none";
 }
