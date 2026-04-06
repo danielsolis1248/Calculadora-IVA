5088f33be0b89dc3ff56794a98deb64fe913658e
danielsolis1248 <itic18.daniel.solis@iunis.edu.mx>
2026-04-06 17:49:44 -0600

    Correcion de funcion de coma 3 digitos en JS

--- calcular.js
+++ calcular.js
@@ -1,19 +1,19 @@
-const input = document.querySelectorAll(".precio");
-
-input.addEventListener('input', (e) => {
-    let valor = e.target.value.replace(/\D/g, "");
-
-    if (valor) {
-        valor = parseInt(valor).toLocaleString('en-US');
-    }
-    e.target.value = valor;
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.querySelector(".precio");
+    input.addEventListener('input', (e) => {
+        let valor = e.target.value.replace(/\D/g, "");
+        if (valor) {
+            valor = parseInt(valor).toLocaleString('en-US');
+        }
+        e.target.value = valor;
+    });
 });
 
 //Funcion para calcular el IVA
 function calcularIVA () {
     //Variables del precio y el portentaje de IVA
-    let precio = document.querySelectorAll('.precio');
-    let porcentajeIVA = document.querySelectorAll('.IVA');
+    let precio = document.querySelector('.precio');
+    let porcentajeIVA = document.querySelector('.IVA');
     
     //Variable para almacenar el IVA del precio original
     let IVA_calculado = porcentajeIVA*precio/100;
