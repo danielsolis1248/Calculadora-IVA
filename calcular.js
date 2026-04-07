5e0c5c0f4be62744b12cf2b10b871a4a2fc578c1
danielsolis1248 <itic18.daniel.solis@iunis.edu.mx>
2026-04-07 15:34:28 -0600

    Obtener valores de comas y limpiar

--- calcular.js
+++ calcular.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     input.addEventListener('input', (e) => {
         let valor = e.target.value.replace(/\D/g, "");
         if (valor) {
-            valor = parseInt(valor).toLocaleString('');
+            valor = parseInt(valor).toLocaleString('en-US');
         }
         e.target.value = valor;
     });
@@ -12,16 +12,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //Funcion para calcular el IVA
 function calcularIVA () {
+    //Obtener valores de comas y limpiar
+    let precioString = document.querySelector('.precio').value.replace(/,/g, "");
+    let IVAString = document.querySelector('.IVA').value.replace(/,/g, "");
+
     //Variables del precio y el portentaje de IVA
-    let precio = parseFloat(document.querySelector('.precio').value);
-    let porcentajeIVA = parseFloat(document.querySelector('.IVA').value);
+    let precio = parseFloat(precioString);
+    let porcentajeIVA = parseFloat(IVAString);
 
     //Validacion de que sean numeros
     if(isNaN(precio) || isNaN(porcentajeIVA)) {
         alert("Ingresa valores validos en ambos campos");
         return;
     }
-    
+
     //Variable para almacenar el IVA del precio original
     let IVA_calculado = precio * (porcentajeIVA / 100);
     
@@ -29,7 +33,7 @@ function calcularIVA () {
     let precioFinal = precio + IVA_calculado;
 
     //Mostrar el resultado en el modal
-    document.getElementById("resultado").textContent = "El precio final de tu producto con IVA es: $" + precioFinal.toFixed(2);
+    document.getElementById("resultado").textContent = "El precio final de tu producto con IVA es: $" + precioFinal.toLocaleString('en-US', {miniumFractionDigits: 2});
     document.getElementById("modal").style.display = "block";
 }
 
